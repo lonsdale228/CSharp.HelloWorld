04b363eb6a67eb3c19f9b6cc59e29ea39e0ef926
Administrator <Administrator@CUPERJOPIK>
2021-12-29 16:51:29 +0200

    Добавьте файлы проекта.

---
+++ ShWan.HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace ShWan.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; //смена цвета в консоле
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(2000); //sleep
+            Console.Clear();// CLR
+            var text = Console.ReadLine();
+            Console.WriteLine(text);
+        }
+    }
+}
